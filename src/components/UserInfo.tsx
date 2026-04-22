63f312fc9fda500d793515e100ff02443102eafe
Alan Herculino <alan.siqueira.25782@aluno.facape.br>
2026-04-22 16:06:15 -0300

    feat: add UserInfo component

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -14,7 +14,7 @@ const UserInfo = ({ user, loading, error }: ProfileProps) => {
       )}
       {loading && <p className="text-[#CDD5E0] text-xl p-8">Loading...</p>}
       {user && (
-        <div className="max-w-4xl ml-60  px-4">
+        <div className="max-w-4xl ml-100  px-4">
           <div className="flex items-end gap-6 relative -mt-12 z-10">
             <div>
               <img
@@ -23,24 +23,24 @@ const UserInfo = ({ user, loading, error }: ProfileProps) => {
                 alt={`Foto de perfil de ${user?.name}`}
               />
             </div>
-            <div className="flex gap-4">
+            <div className="flex gap-5">
               <div className="flex bg-[#111729] p-4 rounded-lg items-center gap-2">
                 <p className="text-[#CDD5E0] text-sm font-bold">Followers</p>
-                <p className="text-[#CDD5E0] text-sm font-bold">
+                <p className="text-[#CDD5E0] font-bold text-base border-l border-[#364153] pl-4">
                   {user.followers}
                 </p>
               </div>
               <div className="flex bg-[#111729] p-4 rounded-xl items-center gap-2">
                 <p className="text-[#CDD5E0] text-sm font-bold">Following</p>
-                <p className="text-[#CDD5E0] text-sm font-bold">
+               <p className="text-[#CDD5E0] font-bold text-base border-l border-[#364153] pl-4">
                   {user.following}
                 </p>
               </div>
 
               <div className="flex  bg-[#111729] p-4 rounded-xl items-center gap-2">
                 <p className="text-[#CDD5E0] text-sm font-bold">Location</p>
-                <p className="text-[#CDD5E0] text-sm font-bold">
-                  {user.location ? user.location : "location not found"}
+                <p className="text-[#CDD5E0] font-bold text-base border-l border-[#364153] pl-4">
+                  {user.location ? user.location : 'location not found'}
                 </p>
               </div>
             </div>
